f93dd74bb2564e261b72f4690a1cf7c223b63b42
piotr <piotr.polakowski92@gmail.l>
2017-10-18 22:58:24 +0200

    V0.2

--- WS.py
+++ WS.py
@@ -1,23 +1,56 @@
-
 from machine import Pin
 from neopixel import NeoPixel
+import math
 from time import sleep
 
-def init():
-    np = NeoPixel(Pin(13),3)
+def test():
+    a = WS_class()
+    a.test()
+
+class WS_class():
+
+    def __init__(self):
+        self.np = NeoPixel(Pin(13), 8)
+        print("inicjacja WS")
+
+    def hsvtorgb(self, h, s, v):
+
+        h = float(h)
+        s = float(s)
+        v = float(v)
+        h60 = h / 60.0
+        h60f = math.floor(h60)
+        hi = int(h60f) % 6
+        f = h60 - h60f
+        p = v * (1 - s)
+        q = v * (1 - f * s)
+        t = v * (1 - (1 - f) * s)
+        r, g, b = 0, 0, 0
+        if hi == 0:
+            r, g, b = v, t, p
+        elif hi == 1:
+            r, g, b = q, v, p
+        elif hi == 2:
+            r, g, b = p, v, t
+        elif hi == 3:
+            r, g, b = p, q, v
+        elif hi == 4:
+            r, g, b = t, p, v
+        elif hi == 5:
+            r, g, b = v, p, q
+        r, g, b = int(r * 255), int(g * 255), int(b * 255)
+        return r, g, b
+
+    def test(self):
+        self.np[0] = (255, 0, 0)
+        self.np[1] = (0, 255, 0)
+        self.np[2] = (0, 0, 255)
+        self.np.write()
+        sleep(10)
+
+        self.np[0] = (0, 0, 0)
+        self.np[1] = (0, 0, 0)
+        self.np[2] = (0, 0, 0)
+        self.np.write()
 
 
-def test(self):
-
-    np[0] = (255,0,0)
-    np[1] = (0,255,0)
-    np[2] = (0,0,255)
-    np.write()
-
-#except KeyboardInterrupt:
- #   print("\n Ctrl-c pressed. Exit")
-def off():
-    np[0] = (0,0,0)
-    np[1] = (0,0,0)
-    np[2] = (0,0,0)
-    np.write()
