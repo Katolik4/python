c472bf164f513c79a32472ee348d25eff00986d8
Katolik4 <piotr.polakowski92@gmail.com>
2018-03-26 16:20:25 +0200

    V0.4.8 - dodano bitmape

---
+++ ESP32/LED_BOARD.py
@@ -0,0 +1,17 @@
+import machine
+import utime
+
+class Led_board()
+
+    def __init__(self):
+        self.led = machine.Pin(2, machine.Pin.OUT)
+        self.led_pwm = machine.PWM(self.led)
+
+    def fade_led(self, czas):
+        while True:
+            for i in range(1024):
+                self.led_pwm.duty(i)
+                utime.sleep_ms(czas)
+            for i in range(1024,-1,-1):
+                self.led_pwm.duty(i)
+                utime.sleep_ms(czas)
--- menu.py
+++ menu.py
@@ -1,30 +1,53 @@
 from config import ConfigPL
 
+
 class Menu():
 
     def __init__(self):
 
         self.cfg = ConfigPL("menu.json")
-        self.json = self.cfg.dict
-        print(self.cfg.dict)
-        print(self.json)
+        self.data = self.cfg.dict
         self.pozycje = 1
         self.podmenu = 1
         self.policz()
 
 
-    def wyswietl(self,menu):
-        print(self.json[menu])
+
+
+    def wyswietltyt(self,menu):
+        print(self.data[menu])
         print("ilosc pozycji", self.pozycje)
         print("ilosc podmenu", self.podmenu)
+        return (self.data[menu])
+
 
     def policz(self):
         x = len(self.cfg.dict)
         self.pozycje = x
+
     def policzpodmenu(self, menu):
-        x = len(self.json[menu])
+        x = len(self.data[menu])
         self.podmenu = x
+
+    def oledprint(self, dane):
+        print(dane)
+
+    def test(self):
+
+        self.oledprint(self.oledtyt())
+
+    def oledtyt(self):
+        for tytul in self.data:
+            self.oledprint(tytul)
+
+    def oledmenu(self, tyt):
+        for poz in self.data[tyt]:
+            self.oledprint(poz)
+
+
 
 if __name__ == '__main__':
     menu = Menu()
-    menu.wyswietl("Ustawienia LED")
+    #menu.wyswietl("Ustawienia LED")
+    menu.test()
+    #menu.oledmenu("Minutnik")
