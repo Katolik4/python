ba9edddf43cf96bbfb33831cd0a9d31309e4d957
Katolik4 <piotr.polakowski92@gmail.com>
2018-03-28 18:42:27 +0200

    V0.4.8 - dodano bitmape

--- LED_BOARD.py
+++ LED_BOARD.py
@@ -6,12 +6,24 @@ class Led_board():
     def __init__(self):
         self.led = machine.Pin(2, machine.Pin.OUT)
         self.led_pwm = machine.PWM(self.led)
+        self.led_pwm.freq(1000)
 
     def fade_led(self, czas):
         while True:
             for i in range(1024):
                 self.led_pwm.duty(i)
                 utime.sleep_ms(czas)
-            for i in range(1024,-1,-1):
+            for i in range(1024, -1, -1):
                 self.led_pwm.duty(i)
                 utime.sleep_ms(czas)
+
+    def pwm(self, pwm):
+        self.led_pwm.duty(pwm)
+
+    def on(self):
+        self.led.on()
+
+    def off(self):
+        self.led.off()
+
+
--- qq.py
+++ qq.py
@@ -1,3 +1,27 @@
-import machine
+import MQTT
 
-def __init__ (self):
+class Mqtt():
+
+    def __init__(self):
+        self.server = "192.168.0.11"
+        self.client = "ESP12E"
+        self.connect()
+
+    def connect(self):
+        self.c = MQTT.MQTTClient(self.client, self.server, 1883, "esp")
+        self.c.connect()
+
+    def sub_cb(self, topic, msg):
+        print((topic, msg))
+
+    def set_cb(self):
+        self.c.set_callback(self.sub_cb)
+    def sub_block(self):
+        self.c.wait_msg()
+
+    def sub_noblock(self):
+        self.c.check_msg()
+
+    def pub(self, topic, msg):
+        self.c.publish(topic, msg)
+
